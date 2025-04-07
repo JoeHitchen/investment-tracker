341ffb1f89104655bf25b8eed88f14c29819b93e
Joe Hitchen <hitchenjoe+git@gmail.com>
2025-04-07 20:29:11 +0100

    Updated fund price retrieval function to also collect the price date

--- sipp/tasks.py
+++ sipp/tasks.py
@@ -1,4 +1,5 @@
-from django.utils import timezone
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -14,12 +15,17 @@ def get_latest_fund_price(fund: sipp.Fund) -> sipp.PricePoint:
     fund_page_soup = BeautifulSoup(response.text, 'html.parser')
     price_line_soup = fund_page_soup.select_one('span.price-divide')
     assert price_line_soup
-
     price_text = price_line_soup.text.strip()
     price_value = int(price_text[:-1].replace('.', '').replace(',', ''))
+
+    price_date_soup = fund_page_soup.select_one('div.price-unavailable')
+    assert price_date_soup
+    price_date_text = price_date_soup.text.strip()
+    price_date = datetime.strptime(price_date_text[13:], '%d %B %Y')
+
     price_point = sipp.PricePoint.objects.create(
         fund=fund,
-        date=timezone.now(),
+        date=price_date,
         price_pence=price_value,
     )
 
